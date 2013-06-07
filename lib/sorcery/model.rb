ce55f74fc1f1c4d165943df3a414b38b5516cafb
robink <robin.komiwes@gmail.com>
2013-06-07 13:40:25 +0200

    more bcrypt removal

--- lib/sorcery/model.rb
+++ lib/sorcery/model.rb
@@ -236,7 +236,7 @@ module Sorcery
           :@email_attribute_name                 => :email,
           :@crypted_password_attribute_name      => :crypted_password,
           :@encryption_algorithm                 => :bcrypt,
-          :@encryption_provider                  => CryptoProviders::BCrypt,
+          :@encryption_provider                  => CryptoProviders::SHA512,
           :@custom_encryption_provider           => nil,
           :@encryption_key                       => nil,
           :@salt_join_token                      => "",
